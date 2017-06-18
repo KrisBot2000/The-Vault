7578e8ea116a97b27574d6157a16c1025a9c1156
Kristin Treder <stuffs@Kristins-MacBook.local>
2017-06-17 16:05:10 -1000

    passes all four tests

--- vault.js
+++ vault.js
@@ -1,4 +1,28 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function(){
+
+  const vault = {};
+
+  function setValue(key, value){
+
+    vault[key] = value;
+    return vault[key];
+  }
+
+  function getValue(key){
+
+    if (key === undefined | vault[key] === undefined){
+      return null;
+    }else{
+      return vault[key];
+    }
+  }
+
+  return{
+    setValue: setValue,
+    getValue: getValue
+  }
+
+
 };
+
